5a4ca2c66bf03eca40ff3e3070f9806b036da2df
Georgi Karapeev <gkarapeev@activtrades.com>
2025-07-08 18:41:30 +0300

    prevent login page if logged in

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { ProfileComponent } from './profile/profile';
 import { authGuard } from './auth.guard';
+import { loginGuard } from './login.guard';
 
 export const routes: Routes = [
-    { path: 'login', component: Login },
-    { path: 'profile', component: ProfileComponent, canActivate: [authGuard] },
-    { path: '', redirectTo: '/profile', pathMatch: 'full' }
+	{ path: 'login', component: Login, canActivate: [loginGuard] },
+	{ path: 'profile', component: ProfileComponent, canActivate: [authGuard] },
+	{ path: '', redirectTo: '/profile', pathMatch: 'full' },
 ];
