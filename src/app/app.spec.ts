8de3a851e16ea25bc6a9e44ba0583c1137e95942
Georgi Karapeev <gkarapeev@activtrades.com>
2025-07-08 17:30:03 +0300

    chore: rename to nopish

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -20,6 +20,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, nonpish-frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, nopish-frontend');
   });
 });
