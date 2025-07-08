b3ea376c95e6f13d44725dad66983205f04ff3d1
Georgi Karapeev <gkarapeev@activtrades.com>
2025-07-08 18:31:44 +0300

    feat: login and guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { Login } from './login/login';
+import { ProfileComponent } from './profile/profile';
+import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
-    { path: 'login', component: Login }
+    { path: 'login', component: Login },
+    { path: 'profile', component: ProfileComponent, canActivate: [authGuard] },
+    { path: '', redirectTo: '/profile', pathMatch: 'full' }
 ];
--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
 	selector: 'app-login',
-	imports: [],
-	template: `<h2>Login</h2>
-		<form>
-			<input type="text" placeholder="Username" />
-			<input type="password" placeholder="Password" />
-			<button type="submit">Login</button>
-		</form>`,
+	imports: [FormsModule],
+	templateUrl: './login.html',
 	styleUrl: './login.scss',
 	standalone: true,
 })
-export class Login {}
+export class Login {
+	username = '';
+	password = '';
+
+	constructor(private router: Router, private authService: AuthService) {}
+
+	login() {
+		if (this.password === '123') {
+			this.authService.login();
+			this.router.navigate(['/profile']);
+		}
+	}
+}
