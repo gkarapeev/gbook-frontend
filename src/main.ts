4f5edc407d03c73dc6e57191070654acdd096eaa
gk_at_gvm <gkarapeev@gmail.com>
2025-07-16 11:13:43 +0300

    chore: todo comment

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,8 @@ import { App } from './pages/root/app';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
+
+
+// Next do do:
+
+// Store JWT and keep us loged-in after refresh
