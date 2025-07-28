837bc8670012c5f0ac22d6dfc3a9e3978e0f9e7a
gk_at_gvm <gkarapeev@gmail.com>
2025-07-28 15:53:30 +0300

    feat: sticky header & disabled empty login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service';
 import { RouterLink } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
 	selector: 'app-login',
-	imports: [RouterLink],
+	imports: [RouterLink, FormsModule],
 	templateUrl: './login.html',
 	styleUrl: './login.scss',
 	standalone: true,
 })
 export class Login {
+	public credentials = {
+		username: '',
+		password: '',
+	};
+
 	constructor(private authService: AuthService) {}
 
-	login(username: string, password: string) {
-		this.authService.login(username, password);
+	login() {
+		this.authService.login(this.credentials.username, this.credentials.password);
 	}
 }
--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
 	selector: 'app-register',
-	imports: [],
+	imports: [RouterLink, FormsModule],
 	templateUrl: './register.html',
 	styleUrl: './register.scss',
 	standalone: true,
