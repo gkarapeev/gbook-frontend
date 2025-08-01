d996eb673862462573ca9bc88323fcfb9043abc6
Gesh <nqma@ti.kaja>
2025-08-01 04:28:12 +0300

    chore: change script

--- scripts/2_copy_new_files.sh
+++ scripts/2_copy_new_files.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-scp ./dist/gbook-frontend/browser/* ross@92.247.116.121:/home/ross/code/gbook-docker/gbook-frontend/dist/gbook-frontend/browser/
+scp ../dist/gbook-frontend/browser/* ross@92.247.116.121:/home/ross/code/gbook-docker/gbook-frontend/dist/gbook-frontend/browser/
