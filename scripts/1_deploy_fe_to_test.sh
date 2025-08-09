4988c7e0a629c5c54567c46c8b146e48f987d595
Gesh <nqma@ti.kaja>
2025-08-09 14:24:15 +0300

    chore: one-click deploy to test

--- scripts/1_deploy_fe_to_test.sh
+++ scripts/1_deploy_fe_to_test.sh
@@ -1,21 +1,11 @@
 #!/bin/sh
 
-# rm -f /usr/share/nginx/html_test/*
-# cp dist/gbook-frontend/browser/* /usr/share/nginx/html_test/
-
-# echo "Deployed FE to test."
-
 BRANCH=test
 REMOTE=ross@gbook.lol
 REMOTE_PUBLIC_DIR=/usr/share/nginx/html_test
 REMOTE_REPO_DIR=/home/ross/code/test_fe_gbook
 
-npm run build:test
-git add .
-git commit -m "chore: build for test"
-git push origin $BRANCH
-
-ssh $REMOTE "
+ssh -t $REMOTE "
   cd $REMOTE_REPO_DIR &&
   git checkout $BRANCH &&
   git pull &&
