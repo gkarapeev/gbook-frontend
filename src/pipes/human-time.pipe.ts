5393000d12e4ab0fe2ccefb905665f0c821a95dc
Gesh <nqma@ti.kaja>
2025-08-30 21:05:23 +0300

    refactor: use a pure pipe to make change detection chill

---
+++ src/pipes/human-time.pipe.ts
@@ -0,0 +1,31 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+	name: 'humanTime',
+	pure: true,
+})
+export class HumanTimePipe implements PipeTransform {
+	transform(timestamp: number): string {
+		if (!timestamp) return '';
+
+		const now = Date.now();
+		const diff = Math.floor((now - timestamp) / 1000);
+
+		if (diff < 3600) {
+            const minutes = Math.floor(diff / 60);
+			return minutes === 0 ? 'just now' : `${minutes}m ago`;
+		}
+
+		if (diff < 86400) {
+			return `${Math.floor(diff / 3600)}h`;
+		}
+
+		if (diff < 2592000) {
+			return `${Math.floor(diff / 86400)}d`;
+		}
+
+		const d = new Date(timestamp);
+		return d.toLocaleDateString() + ' ' + d.toLocaleTimeString();
+	}
+}
+
