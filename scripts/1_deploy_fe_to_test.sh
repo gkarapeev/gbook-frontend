cc828a716a989ccbc3e1e2f62e448a5d3e7a8b84
Gesh <nqma@ti.kaja>
2025-08-30 21:31:11 +0300

    chore: update scripts

--- scripts/1_deploy_fe_to_test.sh
+++ scripts/1_deploy_fe_to_test.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
 
-BRANCH=test
-REMOTE=ross@gbook.lol
-REMOTE_PUBLIC_DIR=/usr/share/nginx/html_test
-REMOTE_REPO_DIR=/home/ross/code/test_fe_gbook
-
-ssh -t $REMOTE "
-  cd $REMOTE_REPO_DIR &&
-  git checkout $BRANCH &&
-  git pull &&
-  doas rm -rf $REMOTE_PUBLIC_DIR/*
-  doas cp -r dist/gbook-frontend/browser/* $REMOTE_PUBLIC_DIR/
-"
-
-echo "Deployed FE to test."
+rm -f /usr/share/nginx/html_test/*
+cp dist/gbook-frontend/browser/* /usr/share/nginx/html_test/
+echo "Deployment done."
--- scripts/1_deploy_fe_to_test_local.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-rm -f /usr/share/nginx/html_test/*
-cp dist/gbook-frontend/browser/* /usr/share/nginx/html_test/
-echo "Deployment done."
